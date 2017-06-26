57ba2a3b2498979d02dbe180f1e63317b061926f
Livia_Santos <liviamelq@gmail.com>
2017-06-26 18:05:34 +1000

    Add basic style

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="panel panel-default">
+      <div className="panel panel-default app-content">
         <div className="panel-body">
           Hello World
         </div>
