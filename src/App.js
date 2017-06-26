945d4e24a737f32e07d8bb3dc1ae332c71594f71
Livia_Santos <liviamelq@gmail.com>
2017-06-26 18:01:44 +1000

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">Hello World</div>
+      <div className="panel panel-default">
+        <div className="panel-body">
+          Hello World
+        </div>
+      </div>
     );
   }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,6 @@
+global.jQuery = require('jquery');
+require('bootstrap');
+import'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
