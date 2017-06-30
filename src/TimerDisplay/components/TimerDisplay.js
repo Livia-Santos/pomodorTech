a0606ce4dc9ba1125180a327c082d5d0bac0f092
Livia_Santos <liviamelq@gmail.com>
2017-07-01 09:56:19 +1000

    Add yarn build, production version

--- src/TimerDisplay/components/TimerDisplay.js
+++ src/TimerDisplay/components/TimerDisplay.js
@@ -10,6 +10,7 @@ const TimerDisplay = (props) => (
     <div className="row center-block">
       {
         (props.timerState === timerStates.COMPLETE)
+        // eslint-disable-next-line
         && <iframe className="center-block youtube-responsive-width" height="315" src="https://www.youtube.com/embed/nrAgK0S9hx4?autoplay=1&start=104&controls=0&showinfo=0"></iframe>
       }
     </div>
